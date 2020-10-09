f6ff80ece03221661d058233cdbb6155de351c6b
Oleg Ivanov <xistadi@gmail.com>
2020-10-10 01:30:17 +0300

    almost working version

--- phone book/Packages/fetch_sqlite.py
+++ phone book/Packages/fetch_sqlite.py
@@ -2,19 +2,22 @@ from sqlalchemy import create_engine
 from sql_cr import Users, Phone_numbers, Base
 from sqlalchemy.orm import sessionmaker
 
+def fetch_sqlite():
+    engine = create_engine('sqlite:///../db/sqlalchemy_phone_book.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker()
+    DBSession.bind = engine
+    session = DBSession()
 
-engine = create_engine('sqlite:///../db/sqlalchemy_phone_book.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker()
-DBSession.bind = engine
-session = DBSession()
+    line = "=" * 80
 
-line = "=" * 80
+    print(f"{line}\nID\tФИО\t\t\tАдрес\t\t\tНомер")
+    query = session.query(Users, Phone_numbers)
+    query = query.join(Users, Users.id == Phone_numbers.user_id)
+    records = query.all()
+    for user, phone in records:
+        print(f"{user.id}\t{user.name}\t\t{user.address}\t\t{phone.phone_number}")
+    print(line)
 
-print(f"{line}\nID\tФИО\t\t\tАдрес\t\t\tНомер")
-query = session.query(Users, Phone_numbers)
-query = query.join(Users, Users.id == Phone_numbers.user_id)
-records = query.all()
-for user, phone in records:
-    print(f"{user.id}\t{user.name}\t\t{user.address}\t\t{phone.phone_number}")
-print(line)
+if __name__ == "__main__":
+    fetch_sqlite()
--- phone book/Packages/phone_book.py
+++ phone book/Packages/phone_book.py
@@ -1,9 +1,9 @@
-from Packages.sql_cr import Users, Phone_numbers, Base
-from Packages.delete_from_db import delete_from_db
-from Packages.fetch_sqlite import fetch_sqlite
-from Packages.insert_phonenumber import insert_phonenumber
-from Packages.insert_sql import insert_sql
-from Packages.update_db import update_db
+from sql_cr import Users, Phone_numbers, Base
+from delete_from_db import delete_from_db
+from fetch_sqlite import fetch_sqlite
+from insert_phonenumber import insert_phonenumber
+from insert_sql import insert_sql
+from update_db import update_db
 
 while True:
 	value = input(f"1.Ввести нового пользователя\n2.Удалить пользователя по id\n3.Обновить данные пользователя\n4.Добавить номер телефона по id\n5.Вывести весь список\nДля выхода введите 'выход'\n")
