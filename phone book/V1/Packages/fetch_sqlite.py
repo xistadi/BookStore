4e3c657c111bd03365e0df208846c93f8d8bdfca
Oleg Ivanov <xistadi@gmail.com>
2020-10-10 21:38:28 +0300

    release version 'V1'

--- phone book/V1/Packages/fetch_sqlite.py
+++ phone book/V1/Packages/fetch_sqlite.py
@@ -1,14 +1,16 @@
 from sqlalchemy import create_engine
-from sql_cr import Users, Phone_numbers, Base
+from Packages.models import Users, Phone_numbers, Base
 from sqlalchemy.orm import sessionmaker
+from Packages.clear_screen import clear_screen
 
 def fetch_sqlite():
-    engine = create_engine('sqlite:///../db/sqlalchemy_phone_book.db')
+    # вывод всех пользователей
+    engine = create_engine('sqlite:///sqlalchemy_phone_book.db')
     Base.metadata.bind = engine
     DBSession = sessionmaker()
     DBSession.bind = engine
     session = DBSession()
-
+    clear_screen()
     line = "=" * 80
 
     print(f"{line}\nID\tФИО\t\t\tАдрес\t\t\tНомер")
@@ -18,6 +20,7 @@ def fetch_sqlite():
     for user, phone in records:
         print(f"{user.id}\t{user.name}\t\t{user.address}\t\t{phone.phone_number}")
     print(line)
+    input("Чтобы подолжить нажмите Enter")
 
 if __name__ == "__main__":
     fetch_sqlite()
--- phone book/V1/Packages/models.py
+++ phone book/V1/Packages/models.py
@@ -5,32 +5,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
-
 Base = declarative_base()
 
 class Users(Base):
+    # класс пользователей (id, name, address)
     __tablename__ = 'users'
-    # Here we define columns for the table users
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     address = Column(String(250), nullable=False)
     phone_numbers = relationship("Phone_numbers", back_populates="user")
 
+    def __str__(self):
+        return self.name
+
 class Phone_numbers(Base):
+    # класс телефонов (id, phone_number)
     __tablename__ = 'phone_numbers'
-    # Here we define columns for the table cars.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     phone_number = Column(Integer)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship(Users, back_populates="phone_numbers")
 
+    def __str__(self):
+        return self.phone_number
 
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
-engine = create_engine('sqlite:///../db/sqlalchemy_phone_book.db')
-
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
+engine = create_engine('sqlite:///sqlalchemy_phone_book.db')
 Base.metadata.create_all(engine)
