478ec932bc14acb3d3c96b466fafe73fe1407cfd
Oleg Ivanov <xistadi@gmail.com>
2020-10-30 19:02:08 +0300

    update/delete book with no img

--- src/products/urls.py
+++ src/products/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('<int:book_pk>/', views.show_book_by_pk_view),
     path('create/', views.create_book_view),
+    path('update/<int:pk>/', views.update_book_view),
+    path('delete/<int:pk>/', views.delete_book_view),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -24,3 +24,30 @@ def create_book_view(request):
     else:
         form = formimport.CreateBookForm()
     return render(request, template_name='products/create_book.html', context={'form': form, 'header': 'book'})
+
+
+def update_book_view(request, pk):
+    if request.method == 'POST':
+        form = formimport.UpdateBookForm(data=request.POST)
+        if form.is_valid():
+            ref_name = form.cleaned_data.get('name')
+            ref_author = form.cleaned_data.get('author')
+            obj = Book.objects.get(pk=pk)
+            obj.name = ref_name
+            obj.description = ref_author
+            obj.save()
+            return HttpResponseRedirect('/references')
+    else:
+        ref = Book.objects.get(pk=pk)
+        form = formimport.UpdateBookForm(data={'name': ref.name, 'author': ref.author})
+    return render(request, template_name='references/update_reference.html', context={'form': form, 'header': 'book'})
+
+
+def delete_book_view(request, pk):
+    if request.method == 'POST':
+        obj = Book.objects.get(pk=pk)
+        obj.delete()
+        return HttpResponseRedirect('/references')
+    else:
+        context = {'header': 'book'}
+    return render(request, template_name='references/delete_reference.html', context=context)
