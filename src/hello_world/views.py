512c5605412a1728dffd9000bb959b576d373831
Oleg Ivanov <xistadi@gmail.com>
2020-11-04 15:47:35 +0300

    paginate for homepage

--- src/hello_world/views.py
+++ src/hello_world/views.py
@@ -1,6 +1,13 @@
-from django.http import HttpResponse
+from django.views.generic import ListView
+from products.models import Book
 from django.shortcuts import render
 
 
+class ShowBookListView(ListView):
+    queryset = Book.objects.order_by('-pk')
+    template_name = 'hello_world/homepage.html'
+    paginate_by = 9
+
+
 def index(request):
-    return render(request, 'hello_world/base.html', context={})
+    return render(request, 'hello_world/homepage.html', context={})
