f5600c876c13f6ec380221e13a1fc74232c56239
Oleg Ivanov <xistadi@gmail.com>
2020-11-09 13:23:20 +0300

    fix singup redirect

--- src/myauth/views.py
+++ src/myauth/views.py
@@ -43,7 +43,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('myaccount')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
