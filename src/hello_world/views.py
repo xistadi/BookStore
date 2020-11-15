3ce5ab4b0b71c2142650fad8d39c7fd108662fc6
Oleg Ivanov <xistadi@gmail.com>
2020-11-15 05:31:08 +0300

    carousel for products

--- src/hello_world/views.py
+++ src/hello_world/views.py
@@ -18,6 +18,10 @@ class ShowBookListView(ListView):
         all_rate = dict(itertools.islice(all_rate.items(), 20))
         base_rate = exchange_rate['base_code']
         context = super().get_context_data(**kwargs)
+        last_some_books_first = Book.objects.order_by('-pk')[:6]
+        last_some_books_second = Book.objects.order_by('-pk')[6:12]
         context['all_rate'] = all_rate
         context['base_rate'] = base_rate
+        context['last_some_books_first'] = last_some_books_first
+        context['last_some_books_second'] = last_some_books_second
         return context
